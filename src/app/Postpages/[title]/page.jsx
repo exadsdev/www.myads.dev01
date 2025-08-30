64ea9ed8ba5d573dfc5a7359ae8f984449905d8e
exadsdev <exadsdev@gmail.com>
2025-08-30 09:47:46 +0700

    first commit

--- src/app/Postpages/[title]/page.jsx
+++ src/app/Postpages/[title]/page.jsx
@@ -2,6 +2,7 @@
 import './posts.css';
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
+import Head from 'next/head';
 
 const PostDetailPage = () => {
   const { title } = useParams(); // รับค่า title จาก URL
@@ -31,22 +32,20 @@ const PostDetailPage = () => {
      <>
         <meta name="google-site-verification" content="ryOmDGaWXKrwl4E9xiLHh2maGVlmpjzyFN8m9QfAyyM" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>post|รับจ้างยิงแอดทำโฆษณา สายเทา Google, Facebook Ads</title>
-        
-        <meta name="title" content="www.myads.dev | รับจ้างยิงแอดทำโฆษณาออนไลน์Google, Facebook Ads" />
+        <title>Post | รับยิงแอด สายเทา ยิงแอดfacebookสายเทา ยิงแอดGoogleสายเทา</title>
+        <meta name="title" content="www.myads.dev | รับจ้างยิงแอดทำโฆษณาออนไลน์ Google, Facebook Ads" />
         <meta name="description" content="www.myads.dev รับจ้างยิงแอดทำโฆษณาออนไลน์รับทำโฆษณา Facebook Adsรับทำโฆษณา Googleทำโฆษณาเว็บไซต์ต่างๆ ด้วยทีมงานมืออาชีพ อัตราค่าบริการเพียงเดือนละ 9,900 บาท" />
-        <meta property="og:title" content="รับจ้างยิงแอดทำโฆษณาออนไลน์Google, Facebook Ads"/>
-        <meta property="og:description" content="รับจ้างยิงแอดทำโฆษณาออนไลน์ รับทำโฆษณา Facebook Ads รับทำโฆษณาGoogleทำโฆษณาเว็บไซต์ต่างๆ ด้วยทีมงานมืออาชีพ อัตราค่าบริการเพียงเดือนละ 9,900 บาท"/>
+        
+        <meta property="og:title" content={post.title}/>
+        <meta property="og:description" content={post.content}/>
         <meta property="og:image" content="https://www.myads.dev/logo.png"/>
         <meta property="og:url" content="https://www.myads.dev"/>
         <meta property="og:type" content="website"/>
 
         <meta name="robots" content="index, follow"/>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-        <link rel="canonical" href="https://www.myads.dev" />
-        <meta name="keywords" content="การยิงแอด, Google, สายเทา, คอร์สเรียนGoogleAdsสายเทา, GoogleAdsสายเทา, สายเทา, บริการคอร์สเรียน, ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,ads,สอนยิงแอด,สอนยิงแอด Conversion,สอนยิงแอดสายเทา,ยิงแอดสายเทา,ยิงโฆษณาสายเทา,รับทำ seo สายเทา,รับทำ seo สายดำ,แอดสายเทา,รับยิงแอดสายเทา,กูเกิ้ลแอดสายเทา,รับทํา google ads,สายเทา seo สายเทา,ตลาดสีเทา,แอดสีเทา,adsสีเทา,กราฟฟิกสายเทา,ยิงแอดเทา,รับยิงแอดfacebookสายเทา,รูปยิงแอดสายเทา,รับจ้างสายเทา,รับโฆษณาสายเทา ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,ads,รับยิงแอด,รับทำโฆษณา,รับโฆษณา,Facebook,การตลาด,โฆษณาออนไลน์,เว็บไซต์,ตลาดเป้าหมาย,โฆษณาบนโซเชียลมีเดีย,Google Ads,การโฆษณาบน Facebook,การโฆษณาออนไลน์บนสื่อต่าง ๆ,การตลาดออนไลน์,การโปรโมท,โฆษณา Facebook,การโฆษณา Google,การโฆษณาสินค้า,การโฆษณาโปรโมชั่น,วิธีการโฆษณา,ความสำเร็จในการตลาด,บริการโฆษณา,การตลาดออนไลน์บน Facebook,การโฆษณาบนเว็บ,การวางแผนโฆษณา,รับจ้างโฆษณา" />
+        <link rel="shortcut icon" href="https://www.myads.dev/favicon.ico" type="image/x-icon"/>
+        <link rel="canonical" href="https://www.myads.dev/Postpages/" />
       </>
-    
     <div className="container mx-auto">
       <div className="card mx-auto mt-5" key={post.id}>
         <div className="title h3 text-center">{post.title}</div>
--- src/app/Postpages/page.jsx
+++ src/app/Postpages/page.jsx
@@ -73,7 +73,7 @@ export default function Page() {
     <>
     
       <>
-      <title>www.myads.dev:PostPage | รับจ้างยิงแอดทำโฆษณาออนไลน์Google, Facebook Ads </title>
+      <title>PostPage|รับจ้างยิงแอดทำโฆษณาออนไลน์Google, Facebook Ads ส่ายเทา </title>
 <meta property="og:title" content="www.myads.dev:PostPage | รับจ้างยิงแอดทำโฆษณาออนไลน์Google, Facebook Ads"/>
 <meta property="og:description" content="รับจ้างยิงแอดทำโฆษณาออนไลน์รับทำโฆษณา Facebook Adsรับทำโฆษณา Googleทำโฆษณาเว็บไซต์ต่างๆ ด้วยทีมงานมืออาชีพ อัตราค่าบริการเพียงเดือนละ 9,900 บาท"/>
 <meta property="og:image" content="https://www.myads.dev/img/ads.jpg"/>
@@ -82,8 +82,10 @@ export default function Page() {
 <meta property="og:type" content="website"/>
 <link rel="shortcut icon" href="https://www.myads.dev/favicon.ico" type="image/x-icon"/> 
 
-<meta name="robots" content="index, follow" />
-<link rel="canonical" href="https://www.myads.dev/PostPage" />
+        <meta name="robots" content="index, follow"/>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+        <link rel="canonical" href="https://www.myads.dev/PostPage" />
+        <meta name="keywords" content="การยิงแอด, Google, สายเทา, คอร์สเรียนGoogleAdsสายเทา, GoogleAdsสายเทา, สายเทา, บริการคอร์สเรียน, ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,ads,สอนยิงแอด,สอนยิงแอด Conversion,สอนยิงแอดสายเทา,ยิงแอดสายเทา,ยิงโฆษณาสายเทา,รับทำ seo สายเทา,รับทำ seo สายดำ,แอดสายเทา,รับยิงแอดสายเทา,กูเกิ้ลแอดสายเทา,รับทํา google ads,สายเทา seo สายเทา,ตลาดสีเทา,แอดสีเทา,adsสีเทา,กราฟฟิกสายเทา,ยิงแอดเทา,รับยิงแอดfacebookสายเทา,รูปยิงแอดสายเทา,รับจ้างสายเทา,รับโฆษณาสายเทา ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,ads,รับยิงแอด,รับทำโฆษณา,รับโฆษณา,Facebook,การตลาด,โฆษณาออนไลน์,เว็บไซต์,ตลาดเป้าหมาย,โฆษณาบนโซเชียลมีเดีย,Google Ads,การโฆษณาบน Facebook,การโฆษณาออนไลน์บนสื่อต่าง ๆ,การตลาดออนไลน์,การโปรโมท,โฆษณา Facebook,การโฆษณา Google,การโฆษณาสินค้า,การโฆษณาโปรโมชั่น,วิธีการโฆษณา,ความสำเร็จในการตลาด,บริการโฆษณา,การตลาดออนไลน์บน Facebook,การโฆษณาบนเว็บ,การวางแผนโฆษณา,รับจ้างโฆษณา" />
 
       </>
 
